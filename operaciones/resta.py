b27546e3f4fd002a74fad48622d6a65669fb11d9
Juan Reina <159836385+juanreina19@users.noreply.github.com>
2025-03-26 21:41:02 -0500

    Delete operaciones directory

--- operaciones/resta.py
+++
@@ -1,4 +0,0 @@
-from operaciones.suma import sumar
-
-def restar(a, b):
-    return sumar(a, sumar(~b, 1))
--- operaciones/suma.py
+++
@@ -1,6 +0,0 @@
-def sumar(a, b):
-    while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
-    return a
